567f9fbe27eaf84fce673e6a7315a9d50bcf707d
unknown <leon_lira_@hotmail.com>
2019-11-29 00:10:03 -0600

    asf

--- src/app/components/cfollow/cfollow.component.ts
+++ src/app/components/cfollow/cfollow.component.ts
@@ -1,79 +1,81 @@
 import { Component, OnInit } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
-import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
-import { Router} from '@angular/router';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment';
 import { FollowService } from '../../services/follow.service';
-import {NgbPopoverConfig} from '@ng-bootstrap/ng-bootstrap';
+import { NgbPopoverConfig } from '@ng-bootstrap/ng-bootstrap';
 @Component({
-  selector: 'app-cfollow',
-  templateUrl: './cfollow.component.html',
-  styleUrls: ['./cfollow.component.scss'],
-  providers: [NgbPopoverConfig]
+	selector: 'app-cfollow',
+	templateUrl: './cfollow.component.html',
+	styleUrls: ['./cfollow.component.scss'],
+	providers: [NgbPopoverConfig]
 })
 export class CfollowComponent implements OnInit {
-	follows:any=[];
-	list:any=[];
-
-	datosgen:any=[];
-	datosempresa:any=[];
-	datosusuarios:any=[];
-		token:string="";
-		sniper:boolean=false;
-		staticAlertClosed:boolean=false;
-		successAlertClosed:boolean=false;
-		alertterror:string="";
-		alertsuccess:string="";
-		palabra:string="";
-  constructor(private config: NgbPopoverConfig,public http:FollowService,private cookieService:CookieService,private modalService: NgbModal,private route:Router)
-  {
-  	 this.config.placement = 'right';
-    this.config.triggers = 'hover';
-  	this.datosgen=JSON.parse(this.cookieService.get('datosUsuario'));
-  	this.datosusuarios=this.datosgen["datosusuario"];
-  	this.datosempresa=this.datosgen["empresa"];
-  	this.token=this.datosgen["Token"];
-  }
-
-  ngOnInit() {
-  	this.sniper=true;
-  	var datos={IDEmpresa:this.datosempresa["IDEmpresa"],token:this.token}
-  	this.http.getfollow(datos)
-  	.subscribe((data)=>{
-  		this.sniper=false;
-  		if(data["response"]["code"]===0){
-  			this.follows=data["response"]["result"];
-  			this.list=this.follows
-  		}
-  		
-  	})
-  }
-  olvidar(id){
-  	this.sniper=true;
-  	var datos={IDEmpresa:this.datosempresa["IDEmpresa"],token:this.token,IDFollow:id}
-  	this.http.olvidar(datos)
-  	.subscribe((data)=>{
-  		this.sniper=false;
-  		if(data["response"]["code"]===0){
-  			this.follows=data["response"]["result"];
-  			this.list=this.follows
-  		}
-  		
-  	})
-  }
-  visitar(ir){
-    this.route.navigateByUrl('/perfilbuscado/'+ir);  
-  }
- busqueda(){
-		if(this.palabra===""){
-  			this.follows=this.list;
-	  	}else{
-	  		this.follows=this.buscapalarabra();
-	  	}
+	follows: any = [];
+	list: any = [];
+	serverruta = environment.urlserver;
+	estados: any[] = [];
+	datosgen: any = [];
+	datosempresa: any = [];
+	datosusuarios: any = [];
+	token = '';
+	sniper = false;
+	staticAlertClosed = false;
+	successAlertClosed = false;
+	alertterror = '';
+	alertsuccess = '';
+	palabra = '';
+	constructor(private config: NgbPopoverConfig, public http: FollowService, private cookieService: CookieService, private modalService: NgbModal, private route: Router) {
+		this.config.placement = 'right';
+		this.config.triggers = 'hover';
+		this.datosgen = JSON.parse(this.cookieService.get('datosUsuario'));
+		this.datosusuarios = this.datosgen['datosusuario'];
+		this.datosempresa = this.datosgen['empresa'];
+		this.token = this.datosgen['Token'];
 	}
-buscapalarabra(){
-  	let usuario=this.follows;
-  		return usuario.filter(usuario=>usuario.Razon_Social.toLocaleLowerCase().includes(this.palabra.toLocaleLowerCase()));
-  }
+
+	ngOnInit() {
+		this.sniper = true;
+		const datos = { IDEmpresa: this.datosempresa['IDEmpresa'], token: this.token };
+		this.http.getfollow(datos)
+			.subscribe((data) => {
+				console.log(data);
+				this.sniper = false;
+				if (data['response']['code'] === 0) {
+					this.estados = data['response']['estados']
+					this.follows = data['response']['result'];
+					this.list = this.follows;
+				}
+
+			});
+	}
+	olvidar(id) {
+		this.sniper = true;
+		const datos = { IDEmpresa: this.datosempresa['IDEmpresa'], token: this.token, IDFollow: id };
+		this.http.olvidar(datos)
+			.subscribe((data) => {
+				this.sniper = false;
+				if (data['response']['code'] === 0) {
+					this.follows = data['response']['result'];
+					this.list = this.follows;
+				}
+
+			});
+	}
+	visitar(ir) {
+		this.route.navigateByUrl('/perfilbuscado/' + ir);
+	}
+	busqueda() {
+		if (this.palabra === '') {
+			this.follows = this.list;
+		} else {
+			this.follows = this.buscapalarabra();
+		}
+	}
+	buscapalarabra() {
+		const usuario = this.follows;
+		return usuario.filter(usuario => usuario.Razon_Social.toLocaleLowerCase().includes(this.palabra.toLocaleLowerCase()));
+	}
 
 }
